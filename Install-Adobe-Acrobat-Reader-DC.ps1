c0ae0fb92cfa01cccf727decd5d32aea5e12e9bb
pxlimited <108461521+pxlimited@users.noreply.github.com>
2022-07-01 09:21:20 +0100

    Add files via upload

--- Install-Adobe-Acrobat-Reader-DC.ps1
+++ Install-Adobe-Acrobat-Reader-DC.ps1
@@ -11,6 +11,51 @@ function Write-Log {
 }
 #endregion
 
+
+# Setup of the required environment variables
+$appName = 'Adobe-Acrobat-Reader-DC'
+$tempDirectory = 'C:\Temp\'
+New-Item `
+    -Path $tempDirectory `
+    -Name $appName `
+    -ItemType Directory `
+    -ErrorAction SilentlyContinue
+$LocalPath = $tempDirectory + $appName
+
+# Update Adobe-Acrobat-Reader-DC
+Write-Host 'AIB Customisation: Downloading the Adobe-Acrobat-Reader-DC app'
+$webSocketsURL = 'https://avdsoftwarestorage.blob.core.windows.net/softwareresource/Adobe-Acrobat-Reader-DC.zip?sp=r&st=2022-07-01T07:56:25Z&se=2023-07-01T15:56:25Z&spr=https&sv=2021-06-08&sr=b&sig=Kx89URy2vRJ0GCaINifq0Y57nfaGi5%2FefxAZWRG%2B%2Bck%3D'
+$downloadedFile = 'Adobe-Acrobat-Reader-DC.zip'
+$outputPath = $LocalPath + '\' + $downloadedFile
+(New-Object System.Net.WebClient).DownloadFile("$webSocketsURL","$outputPath")
+Write-Host 'AIB Customisation: Downloading of the Adobe-Acrobat-Reader-DC app finished'
+
+
+# Adobe Acrobat Reader DC Unzip Parameters & Variables
+$ExpandArchiveParameters = @{
+    Path = $outputPath
+    DestinationPath = $tempDirectory
+    Force = $true
+    Verbose = $true
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+}
+
+    # Adobe Acrobat Reader DC Unzip
+# Unzips the downloaded file to the $appPath directory.
+try {
+    Write-Host -Object "AIB Customisation: Unzipping $downloadedFile."
+    Expand-Archive @ExpandArchiveParameters
+    Write-Host -Object "AIB Customisation: Unzipped $downloadedFile."
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to unzip $downloadedFile."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"
+}
+
+
+
 #region Adobe Acrobat Reader
 try {
     Start-Process -filepath 'C:\temp\AcroRdrDC2200120142_en_US.exe' -Wait -ErrorAction Stop -ArgumentList '/sAll', '/rs', '/msi', 'EULA_ACCEPT=YES'
--- Install-DWG-Trueview-2023.ps1
+++ Install-DWG-Trueview-2023.ps1
@@ -11,6 +11,53 @@ function Write-Log {
 }
 #endregion
 
+
+# Setup of the required environment variables
+$appName = 'Autodesk'
+$tempDirectory = 'C:\Temp\'
+New-Item `
+    -Path $tempDirectory `
+    -Name $appName `
+    -ItemType Directory `
+    -ErrorAction SilentlyContinue
+$LocalPath = $tempDirectory + $appName
+
+
+# Download Autodesk DWG Trueview 2023
+Write-Host 'AIB Customisation: Downloading the Autodesk DWG Trueview 2023 app'
+$webSocketsURL = 'https://avdsoftwarestorage.blob.core.windows.net/softwareresource/DWG-TrueView-2023.zip?sp=r&st=2022-07-01T07:50:30Z&se=2023-07-01T15:50:30Z&spr=https&sv=2021-06-08&sr=b&sig=c09rO8%2BmdspwqaDYdEdLgrzILSULLT41gTA8Hyp6TrQ%3D'
+$downloadedFile = 'DWG-TrueView-2023.zip'
+$outputPath = $LocalPath + '\' + $downloadedFile
+(New-Object System.Net.WebClient).DownloadFile("$webSocketsURL","$outputPath")
+Write-Host 'AIB Customisation: Downloading of the Autodesk DWG Trueview 2023 app finished'
+
+
+# Autodesk Unzip Parameters & Variables
+$ExpandArchiveParameters = @{
+    Path = $outputPath
+    DestinationPath = $tempDirectory
+    Force = $true
+    Verbose = $true
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+}
+
+    # Autodesk Unzip
+# Unzips the downloaded file to the $appPath directory.
+try {
+    Write-Host -Object "AIB Customisation: Unzipping $downloadedFile."
+    Expand-Archive @ExpandArchiveParameters
+    Write-Host -Object "AIB Customisation: Unzipped $downloadedFile."
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to unzip $downloadedFile."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"
+}
+
+
+
+
 #region DWG Trueview
 try {
     Start-Process -filepath C:\temp\Autodesk\Setup.exe -Wait -ErrorAction Stop -ArgumentList '--silent'
