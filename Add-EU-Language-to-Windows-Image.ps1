339f3ef8eadec2f38326cb4c60d5de1d385f2a88
pxlimited <108461521+pxlimited@users.noreply.github.com>
2022-12-08 11:52:07 +0000

    Add files via upload

---
+++ Add-EU-Language-to-Windows-Image.ps1
@@ -0,0 +1,69 @@
+﻿
+########################################################
+## Add Languages to running Windows Image for Capture ##
+########################################################
+
+##Disable Language Pack Cleanup##
+Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Pre-staged app cleanup"
+
+##Set Language Pack Content Stores##
+[string]$LIPContent = "https://avdsoftwarestorage.blob.core.windows.net/px-avd-langpacks"
+
+##Norwegian##
+Add-AppProvisionedPackage -Online -PackagePath $LIPContent/LocalExperiencePack/nb-no/LanguageExperiencePack.nb-NO.Neutral.appx -LicensePath $LIPContent/LocalExperiencePack/nb-no/License.xml
+Add-WindowsPackage -Online -PackagePath $LIPContent/langpacks/Microsoft-Windows-Client-Language-Pack_x64_nb-no.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Basic-nb-no-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Handwriting-nb-no-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-OCR-nb-no-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-TextToSpeech-nb-no-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-MSPaint-FoD-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Notepad-FoD-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-PowerShell-ISE-FOD-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Printing-WFS-FoD-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-StepsRecorder-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-WordPad-FoD-Package~31bf3856ad364e35~amd64~nb-NO~.cab
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Add("nb-no")
+Set-WinUserLanguageList $LanguageList -force
+
+##Spanish##
+Add-AppProvisionedPackage -Online -PackagePath $LIPContent/es-es/LanguageExperiencePack.es-es.Neutral.appx -LicensePath $LIPContent/es-es/License.xml
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Client-Language-Pack_x64_es-es.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Basic-es-es-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Handwriting-es-es-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-OCR-es-es-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Speech-es-es-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-TextToSpeech-es-es-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-MSPaint-FoD-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Notepad-FoD-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-PowerShell-ISE-FOD-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Printing-WFS-FoD-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-StepsRecorder-Package~31bf3856ad364e35~amd64~es-es~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-WordPad-FoD-Package~31bf3856ad364e35~amd64~es-es~.cab
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Add("es-es")
+Set-WinUserLanguageList $LanguageList -force
+
+##French##
+Add-AppProvisionedPackage -Online -PackagePath $LIPContent/fr-fr/LanguageExperiencePack.fr-fr.Neutral.appx -LicensePath $LIPContent/fr-fr/License.xml
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Client-Language-Pack_x64_fr-fr.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Basic-fr-fr-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Handwriting-fr-fr-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-OCR-fr-fr-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-Speech-fr-fr-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-LanguageFeatures-TextToSpeech-fr-fr-Package~31bf3856ad364e35~amd64~~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~fr-fr~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-MSPaint-FoD-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Notepad-FoD-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-PowerShell-ISE-FOD-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-Printing-WFS-FoD-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-StepsRecorder-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+Add-WindowsPackage -Online -PackagePath $LIPContent/Microsoft-Windows-WordPad-FoD-Package~31bf3856ad364e35~amd64~fr-FR~.cab
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Add("fr-fr")
+Set-WinUserLanguageList $LanguageList -force
